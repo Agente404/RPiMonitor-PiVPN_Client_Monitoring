c156d36d307daf2f9ff0757044b84e09810d3feb
Oscar R.C <oscarrc@users.noreply.github.com>
2019-02-19 20:17:35 +0100

    Update pivpn.js

--- pivpn.js
+++ pivpn.js
@@ -1,29 +1,43 @@
 var activePage = GetURLParameter('activePage');
 if (activePage == null){ activePage = 0; }
-                                                                                                                                                                           
+
 function UpdateAddon () {
+  options = '<p>'+
+              'No option available for this addon'+
+            '</p>'
+  $(options).insertBefore("#optionsInsertionPoint")
+
   $.getJSON('dynamic.json', function(data) {
     $.extend(data, getData('static'));
-                                                                                                                                                                           
+
     textToDisplay = "";
     markerString = "Since";
-    textData = data.vpn_clients;                                                                                                                                           
+    textData = data.vpn_clients;
     textArray = textData.trim().slice(textData.indexOf(markerString) + (markerString.length + 6)).replace(/[^ -~]+/g, ";").replace(",",";").split(";");
-    next = 0;                                                                                                                                                              
-                                                                                                                                                                           
-    for (i = 0; i < textArray.length; i++){
-        if ( i == next ){
-          textToDisplay = textToDisplay + "<tr>";
-          next = next + 6;                                                                                                                                                 
-        }                                                                                                                                                                  
-                                                                                                                                                                           
-        textToDisplay = textToDisplay + "<td>" + textArray[i] + "</td>";
-                                                                                                                                                                           
-        if (i == next + 5){
-          textToDisplay = textToDisplay + "</tr>";
-        }                                                                                                                                                                  
-    }                                                                                                                                                                      
-                                                                                                                                                                           
-    $("#addonInsertionPoint").html(textToDisplay);
-  })                                                                                                                                                                       
-}   
+    textArray = textArray.filter(function (el) {
+       return el != "";
+    });
+    next = 0;
+
+    if (textArray[0]!=="No Clients Connected!,"){
+        for (i = 0; i < textArray.length; i++){
+            if ( i == next ){
+              textToDisplay = textToDisplay + "<tr>";
+              next = next + 6;
+            }
+
+            textToDisplay = textToDisplay + "<td>" + textArray[i] + "</td>";
+
+            if (i == next + 5){
+              textToDisplay = textToDisplay + "</tr>";
+            }
+        }
+
+        $("#addonInsertionPoint").html(textToDisplay);
+    }
+  })
+}
+
+$(function () {
+  UpdateAddon();
+});
